cf83418d6cb571fd5d63df8d615e98d58c914e42
https://code.track.uz/a_gaibnazarov <a.gaibnazarov@uzinfocom.uz>
2023-05-05 11:56:19 +0500

    - changed style of the site to admin like page

---
+++ views/layouts/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2023 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <strong class="ml-5">Play Yii2, do anything, imagine</strong>
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.1.0
+    </div>
+</footer>
